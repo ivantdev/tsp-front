fcfe5257f80c2460d483dc6e0e9c3ed0ac48001b
Ivan Castellanos <ivan@ivant.dev>
2022-11-23 23:07:22 -0500

    loading component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,15 +10,17 @@ import { NewTrip } from './NewTrip';
 import { History } from './History';
 import { Account } from './Account';
 import { NavBar } from './NavBar';
+import { Loading } from './Loading';
 import { updateViewportHeight } from '../utilities/updateViewportHeight';
 
 const App = () => {
     const { HomeRoute, MapRoute, NewTripRoute, HistoryRoute, AccountRoute } = useContext(GlobalContext).routes;
-    const { route, UpdateRoute } = useContext(GlobalContext);
+    const { route, UpdateRoute, loading } = useContext(GlobalContext);
     useEffect(UpdateRoute, [route]);
     useEffect(updateViewportHeight, []);
     return (
         <div className="app">
+            { loading && <Loading /> }
             <Routes>
                 <Route path={ HomeRoute } element={<Home />} />
                 <Route path={ MapRoute } element={<MapView />} />
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
+
 const Loading = () => {
+    const { loading } = useContext(GlobalContext);
+
     return (
-        <div className="loading">
-            
+        <div id="loading" className={loading ? "loading active" : "loading"}>
+            <span className="icon">
+                <i className="fa-duotone fa-spinner-third fa-spin"></i>
+            </span>
         </div>
     );
 };
