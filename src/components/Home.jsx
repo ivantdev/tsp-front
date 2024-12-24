3df62afb65e4240471235adeffb29b8fbed808e8
Ivan Castellanos <ivan@ivant.dev>
2024-12-23 21:31:51 -0500

    feat: add application purpose description and enhance Home component styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,6 +23,18 @@ const Home = () => {
                         <img src="https://objects.ivant.dev/public/projects/tsp/imgs/map-map.png" alt="" />
                     </div>
                 </div>
+                <div className="home__text-content">
+                    <h1>App purpose</h1>
+                    <p>This application is designed to solve the Traveling Salesman Problem (TSP), 
+                        a classic optimization challenge in which the objective is to determine the shortest 
+                        possible route that visits a series of locations and returns to the starting point. 
+                        Using advanced algorithms and interactive mapping features, the app provides 
+                        efficient and visually intuitive solutions for logistics, route planning, 
+                        and delivery optimization. Whether you're managing a fleet or simply exploring 
+                        optimal travel paths, this tool is your reliable companion for tackling complex 
+                        routing scenarios.
+                    </p>
+                </div>
             </div>
         </>
     )
